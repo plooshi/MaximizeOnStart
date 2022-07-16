13a27888051bc3ff75c812589de815abe0bc9dc0
plooshi <95927318+plooshi@users.noreply.github.com>
2022-07-16 13:14:48 -0400

    Update and rename src/main/java/net/fabricmc/example/mixin/ExampleMixin.java to src/main/java/xyz/plooshi/startmaximized/MainMixin.java

---
+++ src/main/java/xyz/plooshi/startmaximized/MainMixin.java
@@ -0,0 +1,22 @@
+package xyz.plooshi.startmaximized;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.WindowEventHandler;
+import net.minecraft.client.WindowSettings;
+import net.minecraft.client.util.MonitorTracker;
+import net.minecraft.client.util.Window;
+import org.lwjgl.glfw.GLFW;
+
+// most of this is ripped from sodium tbh
+
+@Mixin(Window.class)
+public class MainMixin {
+	@Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J", shift = At.Shift.BEFORE))
+	private void modifyRequestedContext(WindowEventHandler eventHandler, MonitorTracker monitorTracker, WindowSettings settings, String videoMode, String title, CallbackInfo ci) {
+		GLFW.glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+	}
+}

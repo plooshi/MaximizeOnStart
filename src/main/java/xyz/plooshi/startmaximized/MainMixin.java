5409c5571208280d00f92d92c85397de5a5817e2
plooshi <95927318+plooshi@users.noreply.github.com>
2022-07-16 13:51:59 -0400

    Update MainMixin.java

--- src/main/java/xyz/plooshi/startmaximized/MainMixin.java
+++ src/main/java/xyz/plooshi/startmaximized/MainMixin.java
@@ -1,7 +1,9 @@
 package xyz.plooshi.startmaximized;
 
 import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;

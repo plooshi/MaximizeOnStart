94fd0b2f3874edaedfc7d6f1c343fae79941c641
plooshi <95927318+plooshi@users.noreply.github.com>
2022-07-16 14:10:51 -0400

    Update MainMixin.java

--- src/main/java/xyz/plooshi/startmaximized/MainMixin.java
+++ src/main/java/xyz/plooshi/startmaximized/MainMixin.java
@@ -22,7 +22,7 @@ public abstract class MainMixin {
 	@Final
 	private long handle;
 	
-	@Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J", shift = At.Shift.BEFORE))
+	@Inject(method = "<init>", at = @At(value = "INVOKE_ASSIGN", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J", shift = At.Shift.BEFORE))
 	private void modifyRequestedContext(WindowEventHandler eventHandler, MonitorTracker monitorTracker, WindowSettings settings, String videoMode, String title, CallbackInfo ci) {
 		GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GLFW.GLFW_TRUE);
 		GLFW.glfwMaximizeWindow(this.handle);
